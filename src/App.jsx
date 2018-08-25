a43e242234d81bf48101288bf0d5a6a00ce49f73
Catethysis <catethysis@gmail.com>
2018-08-25 18:56:10 +0300

    Splitted to some logic modules

--- src/App.jsx
+++ src/App.jsx
@@ -23,18 +23,9 @@ let addScript = (script_src) => {
 	document.body.appendChild(script);
 }
 
-export default class App extends React.Component {
-
-	componentDidMount() {
-		addScript("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js");
-		addScript("https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js");
-		addScript("https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js");
-		addScript("https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js");
-		addScript("https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js");
-	}
-	
+class Aside extends React.Component {
 	render () {
-		return [
+		return (
 			<div id="cm-menu">
 				<nav class="cm-navbar cm-navbar-primary">
 					<div class="cm-flex"><a href="index.html" class="cm-logo"></a></div>
@@ -69,7 +60,14 @@ export default class App extends React.Component {
 						</div>
 					</div>
 				</div>
-			</div>,
+			</div>
+		);
+	}
+}
+
+class Header extends React.Component {
+	render() {
+		return (
 			<header id="cm-header">
 				<nav class="cm-navbar cm-navbar-primary">
 					<div class="btn btn-primary md-menu-white hidden-md hidden-lg" data-toggle="cm-menu"></div>
@@ -130,160 +128,203 @@ export default class App extends React.Component {
 						</ul>
 					</div>
 				</nav>
-			</header>,
+			</header>
+		);
+	}
+}
+
+class Panel extends React.Component {
+	render () {
+		return (
+			<div class="container-fluid cm-container-white">
+				<h2 style={{marginTop: 0}}>Welcome to Clearmin template demo !</h2> 
+				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis.</p>
+			</div>
+		)
+	}
+}
+
+class Content extends React.Component {
+	render() {
+		return (
+			<div class="container-fluid">
+				<div class="row cm-fix-height">
+					<div class="col-sm-4">
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<img src={sass_less_png} alt="Less support" class="img-responsive" />
+								<br />
+								<p>Clearmin ships with vanilla CSS, but its source code utilizes Less CSS preprocessor. Quickly get started with precompiled CSS or build on the source. Colors and sizes are easily customizable with less variables.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<img src={devices_png} alt="Responsive across devices" class="img-responsive" />
+								<br />
+								<p>Clearmin easily and efficiently scales your websites and applications with a single code base, from phones to tablets to desktops with CSS media queries. Swipe from left on phones to toggle main menu.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<img src={components_png} alt="Components" class="img-responsive" />
+								<br />
+								<p>With Bootstrap, you get extensive and beautiful documentation for common HTML elements, dozens of custom HTML and CSS components, and awesome jQuery plugins.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row cm-fix-height">
+					<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+						<a href="dashboard-sales.html" class="panel panel-default thumbnail cm-thumbnail">
+							<div class="panel-body text-center">
+								<span class="svg-48">
+									<img src={dashboard_svg} alt="dashboard" />
+								</span>
+								<h4>Dashboard</h4> <small>C3.js charts to display some fancy pies.</small>
+
+							</div>
+						</a>
+					</div>
+					<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+						<a href="notepad.html" class="panel panel-default thumbnail cm-thumbnail">
+							<div class="panel-body text-center">
+								<span class="svg-48">
+									<img src={notepad_svg} alt="notepad" />
+								</span>
+								<h4>Text editor</h4> <small>Summernote WYSIWYG Editor implementation.</small>
+
+							</div>
+						</a>
+					</div>
+					<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+						<a href="components-text.html" class="panel panel-default thumbnail cm-thumbnail">
+							<div class="panel-body text-center">
+								<span class="svg-48">
+									<img src={brick_svg} alt="brick" />
+								</span>
+								<h4>Components</h4> <small>Bootstrap main classes.</small>
+
+							</div>
+						</a>
+					</div>
+					<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+						<a href="layouts-breadcrumb1.html" class="panel panel-default thumbnail cm-thumbnail">
+							<div class="panel-body text-center">
+								<span class="svg-48">
+									<img src={window_layout_svg} alt="window-layout" />
+								</span>
+								<h4>Navbar layouts</h4> <small>Put all you need on the top of your screen.</small>
+
+							</div>
+						</a>
+					</div>
+					<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+						<a href="ico-sf.html" class="panel panel-default thumbnail cm-thumbnail">
+							<div class="panel-body text-center">
+								<span class="svg-48">
+									<img src={cat_svg} alt="cat" />
+								</span>
+								<h4>Icons</h4> <small>Semantic little pics.</small>
+
+							</div>
+						</a>
+					</div>
+					<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+						<a href="login.html" class="panel panel-default thumbnail cm-thumbnail">
+							<div class="panel-body text-center">
+								<span class="svg-48">
+									<img src={lock_open_svg} alt="lock-open" />
+								</span>
+								<h4>Login page</h4> <small>Have a look to the login page.</small>
+
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading">Check our fancy colors</div>
+					<div class="panel-body" id="demo-buttons">
+						<p>Click buttons below to change navbar main color :</p>
+						<div class="row">
+							<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+								<button class="btn btn-block btn-primary" data-switch-color="primary">primary</button>
+								<br />
+							</div>
+							<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+								<button class="btn btn-block btn-success" data-switch-color="success">success</button>
+								<br />
+							</div>
+							<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+								<button class="btn btn-block btn-info" data-switch-color="info">info</button>
+								<br />
+							</div>
+							<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+								<button class="btn btn-block btn-warning" data-switch-color="warning">warning</button>
+								<br />
+							</div>
+							<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+								<button class="btn btn-block btn-danger" data-switch-color="danger">danger</button>
+								<br />
+							</div>
+							<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+								<button class="btn btn-block btn-gray" data-switch-color="gray">gray</button>
+								<br />
+							</div>
+							<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+								<button class="btn btn-block btn-yellow" data-switch-color="yellow">yellow</button>
+								<br />
+							</div>
+							<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+								<button class="btn btn-block btn-purple" data-switch-color="purple">purple</button>
+								<br />
+							</div>
+							<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+								<button class="btn btn-block btn-turquoise" data-switch-color="turquoise">turquoise</button>
+								<br />
+							</div>
+							<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+								<button class="btn btn-block btn-midnight" data-switch-color="midnight">midnight</button>
+								<br />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+class Footer extends React.Component {
+	render () {
+		return (
+			<footer class="cm-footer">
+				<span class="pull-left">Connected as John Smith</span><span class="pull-right">&copy; PAOMEDIA SARL</span>
+			</footer>
+		);
+	}
+}
+
+export default class App extends React.Component {
+	componentDidMount() {
+		addScript("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js");
+		addScript("https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js");
+		addScript("https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js");
+		addScript("https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js");
+		addScript("https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js");
+	}
+	
+	render () {
+		return [
+			<Aside />,
+			<Header />,
 			<div id="global">
-				<div class="container-fluid cm-container-white">
-					<h2 style={{marginTop: 0}}>Welcome to Clearmin template demo !</h2> 
-					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis.</p>
-				</div>
-				<div class="container-fluid">
-					<div class="row cm-fix-height">
-						<div class="col-sm-4">
-							<div class="panel panel-default">
-								<div class="panel-body">
-									<img src={sass_less_png} alt="Less support" class="img-responsive" />
-									<br />
-									<p>Clearmin ships with vanilla CSS, but its source code utilizes Less CSS preprocessor. Quickly get started with precompiled CSS or build on the source. Colors and sizes are easily customizable with less variables.</p>
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-4">
-							<div class="panel panel-default">
-								<div class="panel-body">
-									<img src={devices_png} alt="Responsive across devices" class="img-responsive" />
-									<br />
-									<p>Clearmin easily and efficiently scales your websites and applications with a single code base, from phones to tablets to desktops with CSS media queries. Swipe from left on phones to toggle main menu.</p>
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-4">
-							<div class="panel panel-default">
-								<div class="panel-body">
-									<img src={components_png} alt="Components" class="img-responsive" />
-									<br />
-									<p>With Bootstrap, you get extensive and beautiful documentation for common HTML elements, dozens of custom HTML and CSS components, and awesome jQuery plugins.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="row cm-fix-height">
-						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-							<a href="dashboard-sales.html" class="panel panel-default thumbnail cm-thumbnail">
-								<div class="panel-body text-center">
-									<span class="svg-48">
-										<img src={dashboard_svg} alt="dashboard" />
-									</span>
-									<h4>Dashboard</h4> <small>C3.js charts to display some fancy pies.</small>
-
-								</div>
-							</a>
-						</div>
-						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-							<a href="notepad.html" class="panel panel-default thumbnail cm-thumbnail">
-								<div class="panel-body text-center">
-									<span class="svg-48">
-										<img src={notepad_svg} alt="notepad" />
-									</span>
-									<h4>Text editor</h4> <small>Summernote WYSIWYG Editor implementation.</small>
-
-								</div>
-							</a>
-						</div>
-						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-							<a href="components-text.html" class="panel panel-default thumbnail cm-thumbnail">
-								<div class="panel-body text-center">
-									<span class="svg-48">
-										<img src={brick_svg} alt="brick" />
-									</span>
-									<h4>Components</h4> <small>Bootstrap main classes.</small>
-
-								</div>
-							</a>
-						</div>
-						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-							<a href="layouts-breadcrumb1.html" class="panel panel-default thumbnail cm-thumbnail">
-								<div class="panel-body text-center">
-									<span class="svg-48">
-										<img src={window_layout_svg} alt="window-layout" />
-									</span>
-									<h4>Navbar layouts</h4> <small>Put all you need on the top of your screen.</small>
-
-								</div>
-							</a>
-						</div>
-						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-							<a href="ico-sf.html" class="panel panel-default thumbnail cm-thumbnail">
-								<div class="panel-body text-center">
-									<span class="svg-48">
-										<img src={cat_svg} alt="cat" />
-									</span>
-									<h4>Icons</h4> <small>Semantic little pics.</small>
-
-								</div>
-							</a>
-						</div>
-						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-							<a href="login.html" class="panel panel-default thumbnail cm-thumbnail">
-								<div class="panel-body text-center">
-									<span class="svg-48">
-										<img src={lock_open_svg} alt="lock-open" />
-									</span>
-									<h4>Login page</h4> <small>Have a look to the login page.</small>
-
-								</div>
-							</a>
-						</div>
-					</div>
-					<div class="panel panel-default">
-						<div class="panel-heading">Check our fancy colors</div>
-						<div class="panel-body" id="demo-buttons">
-							<p>Click buttons below to change navbar main color :</p>
-							<div class="row">
-								<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-									<button class="btn btn-block btn-primary" data-switch-color="primary">primary</button>
-									<br />
-								</div>
-								<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-									<button class="btn btn-block btn-success" data-switch-color="success">success</button>
-									<br />
-								</div>
-								<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-									<button class="btn btn-block btn-info" data-switch-color="info">info</button>
-									<br />
-								</div>
-								<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-									<button class="btn btn-block btn-warning" data-switch-color="warning">warning</button>
-									<br />
-								</div>
-								<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-									<button class="btn btn-block btn-danger" data-switch-color="danger">danger</button>
-									<br />
-								</div>
-								<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-									<button class="btn btn-block btn-gray" data-switch-color="gray">gray</button>
-									<br />
-								</div>
-								<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-									<button class="btn btn-block btn-yellow" data-switch-color="yellow">yellow</button>
-									<br />
-								</div>
-								<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-									<button class="btn btn-block btn-purple" data-switch-color="purple">purple</button>
-									<br />
-								</div>
-								<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-									<button class="btn btn-block btn-turquoise" data-switch-color="turquoise">turquoise</button>
-									<br />
-								</div>
-								<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-									<button class="btn btn-block btn-midnight" data-switch-color="midnight">midnight</button>
-									<br />
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<footer class="cm-footer"><span class="pull-left">Connected as John Smith</span><span class="pull-right">&copy; PAOMEDIA SARL</span></footer>
+				<Panel />
+				<Content />
+				<Footer />
 			</div>
 		];
 	}
